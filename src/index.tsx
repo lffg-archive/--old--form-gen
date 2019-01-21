aa2fde796ba3dab634020f4b2f1b8c7794acd417
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-01-20 23:01:18 -0200

    refactor: remove bootstrap from npm. use vendor css file

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Generator from './components/Generator'
 
-import 'bootstrap/dist/css/bootstrap.css'
+import './vendor/bootstrap.min.css'
 
 ReactDOM.render(<Generator />, document.getElementById('root'))
