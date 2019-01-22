ce0cc99c214a4c8e9a2aa366d0403933d95bd7c3
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-01-22 10:45:25 -0200

    day 3

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom'
 
 import './scss/all.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const appRoot = document.getElementById('root')
+ReactDOM.render(<h1 className="title is-1">Hello!</h1>, appRoot)
