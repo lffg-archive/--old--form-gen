a75f04301dc9dbaf4611ca3a7c62a05662ac7fb4
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-01-22 10:29:52 -0200

    day 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
 
-import './vendor/bulma.min.css'
+import './scss/all.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
